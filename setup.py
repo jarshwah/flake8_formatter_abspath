2d407f2f6655b97032bddc986fa1e7c978a50970
Josh Smeaton <josh.smeaton@gmail.com>
2017-04-08 09:15:33 +1000

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         ]
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
