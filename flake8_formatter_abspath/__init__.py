e45511370a83361155ed0176f0ed451320109711
Josh Smeaton <josh.smeaton@gmail.com>
2017-04-08 08:27:32 +1000

    Bump version: 0.1.0 → 0.1.1

--- flake8_formatter_abspath/__init__.py
+++ flake8_formatter_abspath/__init__.py
@@ -5,7 +5,7 @@ from .plugin import AbsolutePathFormatter  # noqa: F401
 
 __author__ = """Josh Smeaton"""
 __email__ = 'josh.smeaton@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = (
     'AbsolutePathFormatter'
 )
